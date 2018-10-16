3b37c16f1511c89b16b0cb2d5dc693357a0451ef
rupdegraff <rupdegraff@chicagoventure.com>
2018-10-16 09:07:02 -0500

    Eliminated some extra lines of code

--- blueprints/sitemap-autogenerator/index.js
+++ blueprints/sitemap-autogenerator/index.js
@@ -1,14 +1,10 @@
 /* eslint-env node */
 var ENV = require(process.cwd() + '/config/environment');
-
 const fs = require('fs');
 
-var baseURL;
-const pathForRouterJS = 'app/router.js';
-var routeArray = [];
-var fileData = '';
-const currentDate = new Date();
+var baseURL, fileData = '', routeArray = [];
 
+const pathForRouterJS = 'app/router.js', currentDate = new Date();
 const header = '<?xml version="1.0" encoding="UTF-8"?>\n' +
   '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
@@ -25,47 +21,23 @@ module.exports = {
       splitDataIntoArrayOfRoutes(data);
     });
   },
-
-  // Is this needed anymore?
-  /*
-  locals: function (options) {
-    fs.readFile(pathForRouterJS, 'utf8', function (err, data) {
-      if (err) return console.log('Encountered the following error:', err);
-
-      let splitBy = /Router.map\(\s*function\s*\(\)\s*\{/; // Provision for variations in spacing for this line
-      data = data.split(splitBy)[1]; // Split file text into code lines by 'Router.map', resulting in two code chunks --> choose the chunk after 'Router.map'
-
-      splitDataIntoArrayOfRoutes(data);
-    });
-
-    return options;
-  },
-
-  afterInstall(options) {}
-  */
 };
 
 function splitDataIntoArrayOfRoutes(data) {
-  let testRE = data.match(/this.route*?[\s\S]*?\;|  [\w\s]+\w*\}\)\;\w*[\s\S]*? | \}\)\;/g);
-
+  let testRE = data.match(/this.route*?[\s\S]*?\;|  [\w\s]+\w*\}\)\;\w*[\s\S]*? | \}\)\;/g); // Looks for matches of this.route*, }); in a line, }); in the beginning of a line
   let nestedPath = [];
 
   testRE.map(function (x, i) { // x is current value, i is index, optional third parameter is the enter array
     if (!testRE[i].match(/\*/g) && !testRE[i].match(/\/\:/)) { // Exclude any route with ':' in the path (for route variable) and any route with '*' in the path
-      // console.log(testRE[i]);
 
       if (testRE[i].match(/\s*function\s*\(\)\s*\{/)) {
         nestedPath.push(checkForQuoteType(testRE[i]));
-        // console.log('*** ', nestedPath);
-        let routeAfterNewNest = testRE[i].split(/\s*function\s*\(\)\s*\{/);
         routeArray.push({
           completeRoute: combineAllPaths(nestedPath),
           path: parseRouteName(testRE[i]),
         });
-        // console.log(routeAfterNewNest[1]);
       } else if (!testRE[i].match(/path\:/) && testRE[i].match(/\s*\}\)\;\s*/)) { // Remove nestedPath from array if end of code block
         nestedPath.pop();
-        // console.log('--- ', nestedPath);
       } else {
         routeArray.push({
           completeRoute: combineAllPaths(nestedPath),
@@ -74,12 +46,9 @@ function splitDataIntoArrayOfRoutes(data) {
       }
     }
   });
-
-  // console.log(routeArray);
   writeToFile();
 }
 
-// New code on 10/11/2018
 function combineAllPaths(pathArray) {
   let path = "";
   pathArray.map(function (x, i) {
